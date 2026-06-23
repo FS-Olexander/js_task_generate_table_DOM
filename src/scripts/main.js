0940d5273b2af5ce2ca6f4396397fe8f1c34763e
Olexander <parfenov4589@gmail.com>
2026-06-23 18:08:34 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const people = require('../api/people.json');
+const people = require('.lib/people.json');
 const table = document.querySelector('.dashboard');
 
 people.forEach((person) => {
